4b5174decc5e2026740f2e3c1f3fb6dfca8fa1dc
Samson <samsondimitrijevic@gmail.com>
2024-03-23 10:24:43 +0100

    sixth commit

--- src/controllers/studentControllers.js
+++ src/controllers/studentControllers.js
@@ -1,15 +1,23 @@
 const { Student } = require("../models/students");
+const {
+  getAllStudentsDB,
+  getStudentByIdDB,
+  getStudentByNameDB,
+  createStudentDB,
+  updateStudentDB,
+  deleteStudentDB,
+} = require("../repositories/studentFunctions");
 
 // GET  http://localhost:3001/students/
 const getAllStudents = async (req, res) => {
-  const students = await Student.find({});
+  const students = await getAllStudentsDB();
   res.status(200).json({ data: students });
 };
 
 // GET http://localhost:3001/students/1
 const getStudentById = async (req, res) => {
   const { id } = req.params;
-  const student = await Student.findById(id);
+  const student = await getStudentByIdDB(id);
   res.status(200).json({ data: student });
 };
 
@@ -20,12 +28,7 @@ const getStudentById = async (req, res) => {
 // GET http://localhost:3001/filter?name=Samson
 const getStudentByName = async (req, res) => {
   const { name } = req.query;
-
-  const nameFilter = {
-    name: { $regex: new RegExp(name, "i") },
-  };
-
-  const student = await Student.find(name ? nameFilter : {});
+  const student = await getStudentByNameDB(name);
   res.status(200).json({ data: student });
 };
 
@@ -41,13 +44,20 @@ const getStudentByName = async (req, res) => {
 
 // POST http://localhost:3001/students/
 const createStudent = async (req, res) => {
-  const newStudent = new Student({
+  const newStudent = await createStudentDB({
     name: req.body.name,
   });
-  await newStudent.save();
-  res.status(201).json({ data: newStudent });
+  res.status(201).json({
+    data: newStudent,
+  });
+};
 
-  /*  console.log(req.body);
+/* const newStudent = new Student({
+    name: req.body.name,
+  });
+  await newStudent.save(); */
+
+/*  console.log(req.body);
   const newStudent = {
     id: Date.now(),
     name: req.body.name,
@@ -56,7 +66,7 @@ const createStudent = async (req, res) => {
   students.push(newStudent);
   res.status(201).json({ data: newStudent }); */
 
-  /*   students.push({
+/*   students.push({
     id: 4,
     name: "Lauren",
   });
@@ -64,14 +74,16 @@ const createStudent = async (req, res) => {
     id: 4,
     name: "Lauren",
   }); */
-};
 
 // PUT
-const updateStudent = (req, res) => {
+const updateStudent = async (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
+  const updatedStudent = await updateStudentDB(id, { name });
+  res.status(200).json({ data: updatedStudent });
+};
 
-  students = students.map((student) => {
+/* students = students.map((student) => {
     if (student.id.toString() === id) {
       return {
         ...student,
@@ -84,22 +96,22 @@ const updateStudent = (req, res) => {
 
   const updatedStudent = students.find(
     (student) => student.id.toString() === id
-  );
-  res.status(200).json({ data: updatedStudent });
-};
+  ); */
 
 // DELETE http://localhost:3001/students/:id
-const deleteStudent = (req, res) => {
+const deleteStudent = async (req, res) => {
   const { id } = req.params;
-
-  students = students.filter((student) => {
-    return student.id.toString() !== id;
-  });
+  
+  deleteStudentDB(id);
 
   res.status(200).json({
     data: "Student Deleted",
   });
 };
+/*  await Student.findByIdAndDelete(id); */
+/*   students = students.filter((student) => {
+    return student.id.toString() !== id;
+  }); */
 
 module.exports = {
   getAllStudents,
